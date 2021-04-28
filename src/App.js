a9e2bffeac951033ac2b0caf2dd386f7ae031bab
Jun Lee <jun@Juns-MacBook-Pro-2.local>
2021-04-27 22:24:01 -0700

    refactoring

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Dropbox from './component/Dropbox'
 const App = () => {
   return (
     <div className="App">
-      <div className='container mx-auto'>
-        <h1>stt</h1>
+      <div className='container mx-auto px-6 mt-6'>
+        <h1 className='text-lg font-medium leading-6'>Speech To Text</h1>
         <Dropbox />
       </div>
     </div>
